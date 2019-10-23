eceac8baea55b5d9df849086a4309d80585edc9b
oddnyk <oddny10@ru.is>
2019-10-23 01:01:29 +0000

    Dagatal

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
-class CalendarScreen extends StatelessWidget {
+import 'package:table_calendar/table_calendar.dart'; /*Oddný bætti þessu vid*/
+
+/*Oddný Bætti þessu vid */
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarController _controller;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CalendarController();
+  }
+
+  @override
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dagatal"),
+        title: Text('Dagatal'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(calendarController: _controller,)
+          ],
+        ),
       ),
     );
   }
