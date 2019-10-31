5c109b579e820d4b4c8d8da109f4df24f95d7e14
Hafsteinn <hafsteinn24@gmail.com>
2019-10-31 11:50:47 +0000

    adding auth.dart

---
+++ lib/services/auth.dart
@@ -0,0 +1,32 @@
+import 'package:husfelagid/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      .map(_userFromFirebaseUser);
+  }
+
+  // sign in with email and password
+
+  // register with email and password
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
