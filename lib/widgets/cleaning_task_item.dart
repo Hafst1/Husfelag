96827f221fd1c82f9eb23c8fff49974e0706e2c1
oddnyk <oddny10@ru.is>
2019-11-03 19:30:05 +0000

    Cleaning Task fixing (not ready)

--- lib/widgets/cleaning_task_item.dart
+++ lib/widgets/cleaning_task_item.dart
@@ -2,29 +2,40 @@ import 'package:flutter/material.dart';
 
 class CleaningTaskItem extends StatelessWidget {
   final String title;
-  final String description;
-  final String route; 
+  final String description; 
 
   CleaningTaskItem(
     {@required this.title,
-    @required this.description,
-    @required this.route});
+    @required this.description});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-    margin: EdgeInsets.symmetric(
-      vertical: 8,
-      horizontal: 5,
-    ),
-    elevation: 5,
-    child: ListTile(
-      contentPadding: EdgeInsets.all(10),
-      title: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
       ),
-    )
+      elevation: 5,
+      child: ListTile(
+        contentPadding: EdgeInsets.all(10),
+        title: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        subtitle: Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    description,
+                    style: TextStyle(fontSize: 15/*, color: Colors.grey[700]*/),
+                  ),
+                ),
+              ],
+            ),
+          ),
+      )
     );
   }
 }
