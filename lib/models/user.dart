90aa853ab76444fd5a9f938df0f1b463823c553f
Hafsteinn <hafsteinn24@gmail.com>
2019-11-27 12:16:52 +0000

    adding that user can change name and address

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -5,3 +5,16 @@ class User {
   User({ this.uid });
 
 }
+
+class UserData {
+
+  final String uid;
+  final String name;
+  final String email;
+  final String home;
+  final String resId;
+  final String apartId;
+
+
+  UserData({ this.uid, this.name, this.email, this.home, this.resId, this.apartId });
+}
--- lib/screens/settings_form.dart
+++ lib/screens/settings_form.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:husfelagid/shared/loading.dart';
+import 'package:provider/provider.dart';
+
+import 'package:husfelagid/services/database.dart';
 import 'package:husfelagid/shared/constants.dart';
+import 'package:husfelagid/models/user.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -13,38 +18,80 @@ class _SettingsFormState extends State<SettingsForm> {
   String _currentName;
   String _currentEmail;
   String _currentHome;
-  String _currentPassword;
+  String _currentResId;
+  String _currentApartId;
+  //String _currentPassword;
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Uppfærðu upplýsingarnar þínar',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Vinsamlegast skráðu nafn' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Vinsamlegast skráðu netfang' : null,
-            onChanged: (val) => setState(() => _currentEmail = val),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Vinsamlegast skráðu heimilisfang' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-        ],
-      ),
+
+    final user = Provider.of<User>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+
+          UserData userData = snapshot.data;
+
+          return Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Uppfærðu upplýsingarnar þínar',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    initialValue: userData.name,
+                    decoration: textInputDecoration.copyWith(hintText: 'Nafn'),
+                    validator: (val) => val.isEmpty ? 'Vinsamlegast skráðu nafn' : null,
+                    onChanged: (val) => setState(() => _currentName = val),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    initialValue: userData.email,
+                    decoration: textInputDecoration.copyWith(hintText: 'Netfang'),
+                    validator: (val) => val.isEmpty ? 'Vinsamlegast skráðu netfang' : null,
+                    onChanged: (val) => setState(() => _currentEmail = val),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    initialValue: userData.home,
+                    decoration: textInputDecoration.copyWith(hintText: 'Heimilisfang'),
+                    validator: (val) => val.isEmpty ? 'Vinsamlegast skráðu heimilisfang' : null,
+                    onChanged: (val) => setState(() => _currentHome = val),
+                  ),
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      'Uppfæra',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async{
+                      if (_formKey.currentState.validate()) {
+                        await DatabaseService(uid: user.uid).updateUserData(
+                          _currentName ?? userData.name,
+                          _currentEmail ?? userData.email,
+                          _currentHome ?? userData.home,
+                          _currentResId ?? userData.resId,
+                          _currentApartId ?? userData.apartId
+                        );
+                        Navigator.pop(context);
+                      }
+                    }
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Loading();
+        }
+        
+      }
     );
   }
 }
