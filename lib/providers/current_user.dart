6d31dd3fcf24c12d1ade2db4436cc9c9b140fe23
brynjargauti <brynjarthorsteins@gmail.com>
2019-11-28 14:58:30 +0000

    add association model + implement form for association creation

--- lib/providers/current_user.dart
+++ lib/providers/current_user.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../models/resident_association.dart';
+
 class CurrentUser with ChangeNotifier {
-  var _id = '';
-  var _email = '';
-  var _name = '';
-  var _home = '';
-  var _residentAssociationNumber = '';
+  String _id = '';
+  String _email = '';
+  String _name = '';
+  String _home = '';
+  String _residentAssociationNumber = '';
+
+  List<ResidentAssociation> _residentAssociations = [];
 
   Future<void> fetchCurrentUser(String id) async {
-    DocumentReference userRef = Firestore.instance
-        .collection("user")
-        .document(id);
+    DocumentReference userRef =
+        Firestore.instance.collection("user").document(id);
     try {
       final fetchedUser = await userRef.get();
       _id = fetchedUser.documentID;
       _email = fetchedUser.data['email'];
       _name = fetchedUser.data['name'];
       _home = fetchedUser.data['home'];
-      _residentAssociationNumber = fetchedUser.data['residentAssociationNumber'];
+      _residentAssociationNumber =
+          fetchedUser.data['residentAssociationNumber'];
     } catch (error) {
       _id = id;
     }
   }
 
-  // Future<void> fetchAssociations() {
-  //   DocumentReference documentRef = Firestore.instance
-  //       .collection("ResidentAssociation");
-  // }
+  Future<void> fetchAssociations() async {
+    CollectionReference associatonRef =
+        Firestore.instance.collection("ResidentAssociation");
+    try {
+      final response = await associatonRef.getDocuments();
+      response.documents.forEach((association) {
+        print(association);
+      });
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> createAssociation() async {
+    try {
+
+    } catch (error) {
+      
+    }
+  }
 
   String getId() {
     return _id;
@@ -52,7 +72,7 @@ class CurrentUser with ChangeNotifier {
   void setId(String id) {
     _id = id;
   }
-  
+
   void setEmail(String email) {
     _email = email;
   }
--- lib/screens/association_registration/create_association_screen.dart
+++ lib/screens/association_registration/create_association_screen.dart
@@ -1,13 +1,168 @@
 import 'package:flutter/material.dart';
 
-class CreateAssociationScreen extends StatelessWidget {
+import '../../widgets/custom_icons_icons.dart';
+import '../../models/resident_association.dart';
+import '../../widgets/save_button.dart';
+import '../../providers/current_user.dart';
+
+class CreateAssociationScreen extends StatefulWidget {
+  @override
+  _CreateAssociationScreenState createState() =>
+      _CreateAssociationScreenState();
+}
+
+class _CreateAssociationScreenState extends State<CreateAssociationScreen> {
+  final _form = GlobalKey<FormState>();
+  var _newAssociation = ResidentAssociation(
+    id: null,
+    name: '',
+    address: '',
+    description: '',
+    accessCode: '',
+  );
+  var _creatorsApartmentNumber = '';
+
+  void _saveForm() async {
+    var isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState.save();
+
+    print(_newAssociation.id);
+    print(_newAssociation.name);
+    print(_newAssociation.address);
+    print(_newAssociation.description);
+    print(_newAssociation.accessCode);
+    print(_creatorsApartmentNumber);
+
+    try {
+
+    } catch (error) {
+
+    }
+
+    // Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Búa til húsfélag'),
+        title: Text('Stofna húsfélag'),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Heiti húsfélags...",
+                  prefixIcon: Icon(CustomIcons.pencil),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Fylla þarf út heiti húsfélags!";
+                  }
+                  if (value.length > 40) {
+                    return "Heiti húsfélags getur ekki verið meira en 40 stafir á lengd!";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _newAssociation = ResidentAssociation(
+                    id: _newAssociation.id,
+                    name: value,
+                    address: _newAssociation.address,
+                    description: _newAssociation.description,
+                    accessCode: _newAssociation.accessCode,
+                  );
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Heimilisfang...",
+                  prefixIcon: Icon(Icons.location_on),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Fylla þarft út heimilisfang!";
+                  }
+                  if (value.length > 40) {
+                    return "Heimilisfang getur ekki verið meira en 40 stafir á lengd!";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _newAssociation = ResidentAssociation(
+                    id: _newAssociation.id,
+                    name: _newAssociation.name,
+                    address: value,
+                    description: _newAssociation.description,
+                    accessCode: _newAssociation.accessCode,
+                  );
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                maxLines: 10,
+                decoration: InputDecoration(
+                  hintText: "Nánari lýsing (valfrjálst)...",
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.text,
+                onSaved: (value) {
+                  _newAssociation = ResidentAssociation(
+                    id: _newAssociation.id,
+                    name: _newAssociation.name,
+                    address: _newAssociation.address,
+                    description: value,
+                    accessCode: _newAssociation.accessCode,
+                  );
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Þitt íbúðarnúmer...",
+                  prefixIcon: Icon(Icons.home),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Fylla þarf út íbúðarnúmer!";
+                  }
+                  if (value.length > 4) {
+                    return "Íbúðarnúmer getur ekki verið lengra en 4 stafir á lengd!";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _creatorsApartmentNumber = value;
+                },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SaveButton(
+                text: 'STOFNA',
+                saveFunc: _saveForm,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
