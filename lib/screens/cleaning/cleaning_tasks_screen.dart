e5019c485b35dcb91fdd38f83b15914abfa5f356
brynjargauti <brynjarthorsteins@gmail.com>
2019-12-01 23:20:30 +0000

    connecting cleaning provider to resident associaton of logged in user + commenting + refactoring

--- lib/screens/cleaning/cleaning_tasks_screen.dart
+++ lib/screens/cleaning/cleaning_tasks_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:husfelagid/providers/cleaning_provider.dart';
 import 'package:provider/provider.dart';
+
+import '../../providers/cleaning_provider.dart';
+import '../../providers/current_user_provider.dart';
 import '../../widgets/cleaning_task_item.dart';
 import 'add_cleaningTask_screen.dart';
 
@@ -8,21 +10,23 @@ class CleaningTasksScreen extends StatefulWidget {
   static const routeName = '/cleaning-tasks';
 
   @override
-  _CleaningTasksScreenState createState() => 
-    _CleaningTasksScreenState();
-  }
+  _CleaningTasksScreenState createState() => _CleaningTasksScreenState();
+}
 
 class _CleaningTasksScreenState extends State<CleaningTasksScreen> {
   var _isInit = true;
   var _isLoading = false;
 
-    void didChangeDependencies() {
+  void didChangeDependencies() {
     if (_isInit) {
       setState(() {
         _isLoading = true;
       });
+      final residentAssociationId =
+          Provider.of<CurrentUserProvider>(context, listen: false)
+              .getResidentAssociationNumber();
       Provider.of<CleaningProvider>(context)
-          .fetchCleaningTasks(context)
+          .fetchCleaningTasks(residentAssociationId, context)
           .then((_) {
         setState(() {
           _isLoading = false;
@@ -35,52 +39,52 @@ class _CleaningTasksScreenState extends State<CleaningTasksScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final PreferredSizeWidget appBar = AppBar(
-      title: Text("Verkefnalisti"),
-    );
     final cleaningTaskData = Provider.of<CleaningProvider>(context);
-    final cleaningTasks = cleaningTaskData.getAllTasks();    
+    final cleaningTasks = cleaningTaskData.getAllTasks();
     return Scaffold(
       appBar: AppBar(
+        title: Text('Verkefnalisti'),
+        centerTitle: true,
       ),
-      body: 
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Card(
-            margin: EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 5,
-            ),
-            child: MaterialButton(
-              color: Colors.white,
-              textColor: Colors.lightBlueAccent,
-              padding: EdgeInsets.all(20.0),
-              child: Text("Bæta við verkefni"),
-              onPressed: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder:(context) => AddCleaningTaskScreen(),
-                  )
-                );
-              },
-            ),
-          ),
-           Expanded(
-            child: ListView.builder(
-              itemCount: cleaningTasks.length,
-              itemBuilder: (ctx, i) => CleaningTaskItem(
-                  id: cleaningTasks[i].id,
-                  title: cleaningTasks[i].title,
-                  description: cleaningTasks[i].description,
-                  taskDone: cleaningTasks[i].taskDone,
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                    Theme.of(context).primaryColor),
+              ),
             )
-          )
-          ),
-        ],
-        
-      ),
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: MaterialButton(
+                    color: Colors.white,
+                    textColor: Colors.lightBlueAccent,
+                    padding: EdgeInsets.all(20.0),
+                    child: Text("Bæta við verkefni"),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => AddCleaningTaskScreen(),
+                      ));
+                    },
+                  ),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: cleaningTasks.length,
+                        itemBuilder: (ctx, i) => CleaningTaskItem(
+                              id: cleaningTasks[i].id,
+                              title: cleaningTasks[i].title,
+                              description: cleaningTasks[i].description,
+                              taskDone: cleaningTasks[i].taskDone,
+                            ))),
+              ],
+            ),
     );
   }
 }
