5a44e26c2273af7edf8df392938b6b889d8d0712
Hafsteinn <hafsteinn24@gmail.com>
2019-12-06 13:49:32 +0000

    refactor

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -63,6 +63,7 @@ class AuthService {
       await user.updatePassword(password);
     } on Exception catch (error) {
       print('Password can\'t be changed' + error.toString());
+      return null;
     }
   }
 
