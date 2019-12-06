adc800cab8a2dc4e70b72cea34ce777951acbb3b
Hafsteinn <hafsteinn24@gmail.com>
2019-12-06 15:45:50 +0000

    redfactoring

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../services/auth.dart';
-//import 'package:husfelagid/screens/settings_form.dart';
 import '../screens/profile_page.dart';
 import '../widgets/home_option.dart';
 import '../screens/constructions/constructions_screen.dart';
@@ -15,28 +14,18 @@ class HomeScreen extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-
-    // void _showSettingsPanel() {
-    //   showModalBottomSheet(context: context, builder: (context) {
-    //     return Container(
-    //       padding: EdgeInsets.symmetric(vertical: 20.0, horizontal:60.0),
-    //       child: SettingsForm(),
-    //     );
-    //   });
-    // }
-
     void choiceAction(String choice) {
-    if (choice == Constants.MyPage){
-      //_showSettingsPanel();
-      Navigator.of(context).push(
+      if (choice == Constants.MyPage) {
+        //_showSettingsPanel();
+        Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) => ProfilePage(),
           ),
         );
-    } else if (choice == Constants.SignOut) {
-      _auth.signOut();
+      } else if (choice == Constants.SignOut) {
+        _auth.signOut();
+      }
     }
-  }
 
     return Scaffold(
       appBar: AppBar(
@@ -44,39 +33,15 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         actions: <Widget>[
           PopupMenuButton<String>(
-            onSelected: choiceAction,
-            itemBuilder: (BuildContext context) {
-              return Constants.choices.map((String choice) {
-                return PopupMenuItem<String> (
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            }
-          ),
-          /*FlatButton.icon(
-            icon: Icon(Icons.person, color: Colors.white),
-            label: Text(
-              'Skrá út',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            onPressed: () async {
-              await _auth.signOut();
-            }),
-          FlatButton.icon(
-            icon: Icon(Icons.settings, color: Colors.white),
-            label: Text(
-              'Settings',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            onPressed: () => _showSettingsPanel(),
-          )*/
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context) {
+                return Constants.choices.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              }),
         ],
       ),
       body: GridView(
