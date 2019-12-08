fd91f89ccf70b2bab42ec0afba1247091309b501
brynjargauti <brynjarthorsteins@gmail.com>
2019-12-08 04:59:19 +0000

    minor change - removed unneccessary import

--- lib/providers/documents_provider.dart
+++ lib/providers/documents_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/services.dart';
 import 'dart:io';
 
 import '../models/folder.dart';
