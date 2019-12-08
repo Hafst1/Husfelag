77e3d26a5c64a5415dcc0b1d019f9139721c90ea
Hafsteinn <hafsteinn24@gmail.com>
2019-12-08 16:16:43 +0000

    refactor

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import '../services/auth.dart';
