7b56fc6120912c283b3ef95b0367444e9f56f60e
brynjargauti <brynjarthorsteins@gmail.com>
2019-12-12 03:45:14 +0000

    test for main options in associaton options screen

---
+++ test/association_options_screen_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/screens/association_registration/association_options_screen.dart';
+
+void main() {
+  testWidgets('should display the four main option buttons',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: AssociationOptionsScreen(),
+      ),
+    );
+
+    var createAssociationButton = find.text('Stofna húsfélag');
+    expect(createAssociationButton, findsOneWidget);
+
+    var joinAssociationButton = find.text('Ganga í húsfélag');
+    expect(joinAssociationButton, findsOneWidget);
+  });
+}
