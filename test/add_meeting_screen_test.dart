d1c7a7ff7b68dafda219ec7201c741d99d5b9891
brynjargauti <brynjarthorsteins@gmail.com>
2019-12-12 18:07:05 +0000

    re-added tests that were removed with a little duration

--- test/add_meeting_screen_test.dart
+++ test/add_meeting_screen_test.dart
@@ -18,35 +18,35 @@ final date = DateFormat.yMMMMEEEEd()
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   group('add meeting form testing:', () {
-    // testWidgets('should display error messages on no input',
-    //     (WidgetTester tester) async {
-    //   await tester.pumpWidget(
-    //     MultiProvider(
-    //       providers: [
-    //         ChangeNotifierProvider.value(
-    //           value: CurrentUserProvider(),
-    //         ),
-    //         ChangeNotifierProvider.value(
-    //           value: MeetingsProvider(),
-    //         ),
-    //       ],
-    //       child: MaterialApp(
-    //         home: AddMeetingScreen(),
-    //       ),
-    //     ),
-    //   );
+    testWidgets('should display error messages on no input',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider.value(
+              value: CurrentUserProvider(),
+            ),
+            ChangeNotifierProvider.value(
+              value: MeetingsProvider(),
+            ),
+          ],
+          child: MaterialApp(
+            home: AddMeetingScreen(),
+          ),
+        ),
+      );
 
-    //   var button = find.text('BÓKA');
-    //   expect(button, findsOneWidget);
+      var button = find.text('BÓKA');
+      expect(button, findsOneWidget);
 
-    //   await tester.tap(button);
-    //   await tester.pump();
+      await tester.tap(button);
+      await tester.pump(Duration(seconds: 1));
 
-    //   expect(find.text('Fylla þarf út titil fundar!'), findsOneWidget);
-    //   expect(find.text('Útvega þarf dagsetningu fundar!'), findsOneWidget);
-    //   expect(find.text('Útvega þarf tímasetningu!'), findsNWidgets(2));
-    //   expect(find.text('Fylla þarf út staðsetningu fundar!'), findsOneWidget);
-    // });
+      expect(find.text('Fylla þarf út titil fundar!'), findsOneWidget);
+      expect(find.text('Útvega þarf dagsetningu fundar!'), findsOneWidget);
+      expect(find.text('Útvega þarf tímasetningu!'), findsNWidgets(2));
+      expect(find.text('Fylla þarf út staðsetningu fundar!'), findsOneWidget);
+    });
 
     testWidgets('should display no error messages on valid input',
         (WidgetTester tester) async {
@@ -93,55 +93,55 @@ void main() {
       expect(find.text('Fylla þarf út staðsetningu fundar!'), findsNothing);
     });
 
-    // testWidgets('should display error messages on invalid input',
-    //     (WidgetTester tester) async {
-    //   await tester.pumpWidget(
-    //     MultiProvider(
-    //       providers: [
-    //         ChangeNotifierProvider.value(
-    //           value: CurrentUserProvider(),
-    //         ),
-    //         ChangeNotifierProvider.value(
-    //           value: MeetingsProvider(),
-    //         ),
-    //       ],
-    //       child: MaterialApp(
-    //         home: AddMeetingScreen(),
-    //       ),
-    //     ),
-    //   );
+    testWidgets('should display error messages on invalid input',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider.value(
+              value: CurrentUserProvider(),
+            ),
+            ChangeNotifierProvider.value(
+              value: MeetingsProvider(),
+            ),
+          ],
+          child: MaterialApp(
+            home: AddMeetingScreen(),
+          ),
+        ),
+      );
 
-    //   var textFormFields = find.byType(TextFormField);
-    //   expect(textFormFields, findsNWidgets(6));
+      var textFormFields = find.byType(TextFormField);
+      expect(textFormFields, findsNWidgets(6));
 
-    //   var titleTextField = textFormFields.at(0);
-    //   var timeFromTextField = textFormFields.at(2);
-    //   var timeToTextField = textFormFields.at(3);
-    //   var locationTextField = textFormFields.at(4);
+      var titleTextField = textFormFields.at(0);
+      var timeFromTextField = textFormFields.at(2);
+      var timeToTextField = textFormFields.at(3);
+      var locationTextField = textFormFields.at(4);
 
-    //   var button = find.text('BÓKA');
-    //   expect(button, findsOneWidget);
+      var button = find.text('BÓKA');
+      expect(button, findsOneWidget);
 
-    //   // meeting starts at 20:00 but ends at 18:00
-    //   await tester.enterText(timeFromTextField, '20:00');
-    //   await tester.enterText(timeToTextField, '18:00');
-    //   await tester.enterText(
-    //       titleTextField, 'invalid string with more than 40 letters!');
-    //   await tester.enterText(
-    //       locationTextField, 'invalid string with more than 40 letters!');
+      // meeting starts at 20:00 but ends at 18:00
+      await tester.enterText(timeFromTextField, '20:00');
+      await tester.enterText(timeToTextField, '18:00');
+      await tester.enterText(
+          titleTextField, 'invalid string with more than 40 letters!');
+      await tester.enterText(
+          locationTextField, 'invalid string with more than 40 letters!');
 
-    //   await tester.tap(button);
-    //   await tester.pump();
+      await tester.tap(button);
+      await tester.pump(Duration(seconds: 1));
 
-    //   expect(find.text('Ógild tímasetning!'), findsNWidgets(2));
-    //   expect(
-    //       find.text(
-    //           'Titill fundar getur ekki verið meira en 40 stafir á lengd!'),
-    //       findsOneWidget);
-    //   expect(
-    //       find.text(
-    //           'Staðsetning fundar getur ekki verið meira en 40 stafir á lengd!'),
-    //       findsOneWidget);
-    // });
+      expect(find.text('Ógild tímasetning!'), findsNWidgets(2));
+      expect(
+          find.text(
+              'Titill fundar getur ekki verið meira en 40 stafir á lengd!'),
+          findsOneWidget);
+      expect(
+          find.text(
+              'Staðsetning fundar getur ekki verið meira en 40 stafir á lengd!'),
+          findsOneWidget);
+    });
   });
 }
